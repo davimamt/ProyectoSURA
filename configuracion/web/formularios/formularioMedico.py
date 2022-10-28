78a1f418f2779e97ea73eed1cd34e2749e674eae
511 <511@academicos.cesde.edu.co>
2022-10-28 17:39:58 -0500

    interfaz medicos

---
+++ configuracion/web/formularios/formularioMedico.py
@@ -0,0 +1,32 @@
+
+from django import forms 
+class FormularioMedico(forms.Form):
+
+    ESPECIALIDADES = (
+        (1, 'Cardiología'),
+        (2, 'Medicina Interna'),
+        (3, 'Medico General'),
+        (4, 'Ortopedia'),
+        (5, 'Pediatria'),
+    )
+    JORNADAS = (
+        (1, '6:00 - 14:00'),
+        (2, '14:00 - 22:00'),
+        (3, '22:00 - 6:00'),
+
+    )
+    SEDES = (
+        (1, 'Almacentro'),
+        (2, 'Punto Clave'),
+        (3, 'Molinos'),
+
+    )
+
+    nombre = forms.CharField()
+    apellidos = forms.CharField()
+    cedula = forms.CharField()
+    tarjetaProfesional = forms.CharField()
+    especialidad = forms.ChoiceField()
+    jornada = forms.ChoiceField()
+    contacto = forms.CharField()
+    sede = forms.ChoiceField()
--- configuracion/web/views.py
+++ configuracion/web/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 #Render es PINTAR 
 def Home(request):
-    return render(request, 'index.html') 
+    return render(request, 'index.html')
+
+def Medicos(request):
+    return render(request, 'registrosmedicos.html')
+
